cf0b460fef9f320102381892f3a1bb6a09c4a760
ihsan <ihsanpuntadewa@gmail.com>
2024-05-29 09:30:55 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Livewire\Chat;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
+Route::get('dashboard', function () {
+    return view('dashboard', [
+        'users' => User::where('id', '!=', auth()->id())->get(),
+    ]);
+})
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
@@ -12,4 +18,6 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+Route::get('/chat/{user}', Chat::class)->name('chat');
+
+require __DIR__ . '/auth.php';
